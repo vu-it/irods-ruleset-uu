96584bd072863de0a2be18aa0880808a05d9de39
Lazlo Westerhof <l.r.westerhof@uu.nl>
2018-01-31 21:21:48 +0100

    Skip replication if object does not exist (any more).

--- uuReplicate.r
+++ uuReplicate.r
@@ -1,7 +1,7 @@
 # \file
 # \brief replication functions
 # \author Ton Smeele
-# \copyright Copyright (c) 2015, Utrecht university. All rights reserved
+# \copyright Copyright (c) 2015-2018, Utrecht University. All rights reserved.
 # \license GPLv3, see LICENSE
 #
 #
@@ -19,17 +19,33 @@
 # 
 # \param[in] object	       data object to be replicated
 # \param[in] sourceResource    resource to be used as source
-# \param[in] targetResource    resource to be used as destination
-#
-#      
-# NB: this rule uses remote() as workaround for github.com/irods/irods issue:
-#     delay() in dynamic pep crashes agent  #3342
-#                        
+# \param[in] targetResource    resource to be used as destination 
 uuReplicateAsynchronously(*object, *sourceResource, *targetResource) {
-   remote("localhost","") {
-      delay("<PLUSET>1s</PLUSET><EF>1m DOUBLE UNTIL SUCCESS OR 10 TIMES</EF>") {
-         *options = "rescName=*sourceResource++++destRescName=*targetResource";
-         msiDataObjRepl(*object, *options, *status);
-      }
-   }
+        delay("<PLUSET>1s</PLUSET><EF>1m DOUBLE UNTIL SUCCESS OR 10 TIMES</EF>") {
+		# Find object to replicate.
+                uuChopPath(*object, *parent, *basename);
+                *objectId = 0;
+	        *found = false;
+
+	        foreach(*row in SELECT DATA_ID, DATA_MODIFY_TIME, DATA_OWNER_NAME, DATA_SIZE, COLL_ID
+			WHERE DATA_NAME      = *basename
+			AND   COLL_NAME      = *parent
+			AND   DATA_RESC_NAME = *sourceResource
+		       ) {
+		        if (!*found) {
+			        *found = true;
+			        break;
+                        }
+	        }
+
+		# Skip replication if object does not exists (any more).
+	        if (!*found) {
+		        writeLine("serverLog", "uuReplicateAsynchronously: DataObject was not found.");
+		        succeed;
+	        }
+
+		# Replicate object to target resource.
+		*options = "rescName=*sourceResource++++destRescName=*targetResource";
+                msiDataObjRepl(*object, *options, *status);
+        }
 }
